6ecaea24a34342e75100e42289428ce4fda1a9af
James McEneaney <jamesmceneaney17@gmail.com>
2023-04-30 21:17:05 +0100

    addition of code which produces a histogram using seaborn

--- analysis.py
+++ analysis.py
@@ -6,4 +6,13 @@ import pandas as pd
 import seaborn as sns
 import sys
 
- 
+# add headings to the columns
+# reference: https://realpython.com/python-csv/#reading-csv-files-with-csv
+data = pd.read_csv("iris.data.csv", names=['sepal length', 'sepal width', 'petal length', 'petal width', 'variety'])
+
+#print(data)
+
+sns.set(style="darkgrid")
+df = sns.load_dataset('iris')
+sns.histplot(df['petal_length'],kde = False)
+plt.show()

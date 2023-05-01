ff2673e306ef9ba2cad721c4df11796f78a727db
James McEneaney <jamesmceneaney17@gmail.com>
2023-05-01 21:56:34 +0100

    addition of code to generate meta-data about data-set plus code for generating histograms and a pairplot using seaborn

--- analysis.py
+++ analysis.py
@@ -7,12 +7,45 @@ import seaborn as sns
 import sys
 
 # add headings to the columns
-# reference: https://realpython.com/python-csv/#reading-csv-files-with-csv
-data = pd.read_csv("iris.data.csv", names=['sepal length', 'sepal width', 'petal length', 'petal width', 'variety'])
+# reference: https://realpython.com/python-csv/#reading-csv-files-with-pandas 
+df = pd.read_csv("iris.data.csv", names=['sepal length', 'sepal width', 'petal length', 'petal width', 'variety'])
 
-#print(data)
+# print(df)
 
-sns.set(style="darkgrid")
-df = sns.load_dataset('iris')
-sns.histplot(df['petal_length'],kde = False)
-plt.show()
+# the first ten rows of the dataframe
+# print(df.head(10))
+
+# general overview of dataframe
+# print(df.info())
+
+# the index labels of the dataframe
+# print(df.index)
+
+# the column lables of the dataframe
+# print(df.columns)
+
+# numpy representation of the dataframe
+# print(df.values)
+
+# size of dataframe (number of elements in it)
+# print(df.size)
+
+
+#sns.set(style="darkgrid")
+#sns.displot(df, x = 'petal length', kde = False)
+#plt.savefig('pet_len_hist.png')
+
+#sns.set(style="darkgrid")
+#sns.displot(df, x = 'petal width', kde = False)
+#plt.savefig('pet_wid_hist.png')
+
+#sns.set(style="darkgrid")
+#sns.displot(df, x = 'sepal length', kde = False)
+#plt.savefig('sep_len_hist.png')
+
+#sns.set(style="darkgrid")
+#sns.displot(df, x = 'sepal width', kde = False)
+#plt.savefig('sep_wid_hist.png')
+
+sns.pairplot(df)
+plt.savefig('iris_pairplot.png')

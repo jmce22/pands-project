22b2a124d4c80c3e8e84c86047eddc7d1478b9aa
James McEneaney <jamesmceneaney17@gmail.com>
2023-05-05 23:03:11 +0100

    addition and removal of some code relating to the summary statistics of the data set.

--- analysis.py
+++ analysis.py
@@ -16,7 +16,7 @@ df = pd.read_csv("iris.data.csv", names=['sepal length', 'sepal width', 'petal l
 
 # To print the summary statistics to a file, we create a function where all of the print statements contained within get printed to
 # the text file when the function is called. This will allow us to just print the summary statistics to the text file and none of the
-# other output from the terminal.
+# other output from the script which will be in the terminal.
 def summary_statistics():
 
 
@@ -25,27 +25,41 @@ def summary_statistics():
     f = open("summary statistics.txt", 'w')
     sys.stdout = f
 
-# The groupyby function in pandas allows us to split the dataframe into groups separated according to the data in the column 'variety'. 
+    summary_stats = df.describe()
+    
+    
+# This firstly transposes the output of the .describe() method, to make it easier to read it when it is printed out. 
+# References: https://numpy.org/doc/stable/reference/generated/numpy.transpose.html 
+
+
+    transpose_summary = np.transpose(summary_stats)
+    summary= transpose_summary
+
+    
+# The groupyby function in pandas allows us to split the dataframe into groups separated according to the data in the 
+# column with heading 'variety'. 
 # We can then generate summary statistics for each class of Iris in the dataset.
 # Reference: https://www.geeksforgeeks.org/python-pandas-dataframe-groupby/ 
-    summary_statistics_class = df.groupby("variety").describe()
+    
+    summary_stats_by_class = df.groupby("variety").describe()
+    transpose_summary_class = np.transpose(summary_stats_by_class)
+    summary_by_class= transpose_summary_class
 
-
-# This firstly transposes the output of the .describe() method, to make it easier to read it when it is printed out. 
-# This transposed output is then converted into a string, to allow us to print it to a text file using 'with open'.
-# Reference: https://www.digitalocean.com/community/tutorials/numpy-matrix-transpose-array#transpose-of-an-array-like-object
-# Reference: https://www.geeksforgeeks.org/python-pandas-dataframe-to_string/
-
-
-    transpose_summary_class = np.transpose(summary_statistics_class)
-    summary_class_string= transpose_summary_class.to_string()
-
-    print("Here are some summary statistics for each class of Iris flower: \n ")
-    print(summary_class_string)
+    print("Summary statistics for the four variables measured for the Iris data set")
+    print("\n\n")
+    print(summary)
+    print("\n\n")
+    print("Here are some summary statistics for each class of Iris flower: \n")
+    print(summary_by_class)
+    print("\n")
+    print("For a description of these summary statistics, please see the README file for this project.")
 
     f.close()
 
+# this calls the function defined above
 summary_statistics()
+
+
 
 # the first ten rows of the dataframe
 # print(df.head(10))
@@ -68,17 +82,17 @@ summary_statistics()
 # summary statistics
 # print(df.describe())
 
-#df.hist(bins = 20)
-#plt.savefig('histograms.png')
-#plt.show()
+# df.hist(bins = 20)
+# plt.savefig('histograms.png')
+# plt.show()
 
 #df.hist(x = 'petal length', y = 'iris setosa')
 #plt.show()
 
-#sns.set(style="darkgrid")
-#sns.displot(df, x = 'petal length', kde = False)
-#plt.savefig('petal_length_hist.png')
-#plt.show()
+sns.set(style="darkgrid")
+sns.displot(df, x = 'petal length', kde = False)
+plt.savefig('petal_length_hist.png')
+plt.show()
 
 #sns.set(style="darkgrid")
 #sns.displot(df, x = 'petal width', kde = False)

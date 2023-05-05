a0541a4fa70498760c0e8fa69d20df3950dcc91b
James McEneaney <jamesmceneaney17@gmail.com>
2023-05-05 14:25:07 +0100

    addition of function containing sys.stdout to print termial output for summary statistics to a text file

--- analysis.py
+++ analysis.py
@@ -4,19 +4,31 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import seaborn as sns
+
+# to redirect output from command line onto a text file, we need to import the sys module.
 import sys
+
 
 # add headings to the columns
 # reference: https://realpython.com/python-csv/#reading-csv-files-with-pandas 
 df = pd.read_csv("iris.data.csv", names=['sepal length', 'sepal width', 'petal length', 'petal width', 'variety'])
 
-# print(df)
+
+# To print the summary statistics to a file, we create a function where all of the print statements contained within get printed to
+# the text file when the function is called. This will allow us to just print the summary statistics to the text file and none of the
+# other output from the terminal.
+def summary_statistics():
 
 
-# This allows us to split the dataframe into groups separated according to the data in the column 'variety'. We can then generate
-# summary statistics for each class of Iris in the dataset
+# The function sys.stdout allows us to redirect the print statements contained within it to a text file
+# Reference: https://stackoverflow.com/questions/23364096/how-to-write-output-of-terminal-to-file
+    f = open("summary statistics.txt", 'w')
+    sys.stdout = f
+
+# The groupyby function in pandas allows us to split the dataframe into groups separated according to the data in the column 'variety'. 
+# We can then generate summary statistics for each class of Iris in the dataset.
 # Reference: https://www.geeksforgeeks.org/python-pandas-dataframe-groupby/ 
-summary_statistics_class = df.groupby("variety").describe()
+    summary_statistics_class = df.groupby("variety").describe()
 
 
 # This firstly transposes the output of the .describe() method, to make it easier to read it when it is printed out. 
@@ -24,11 +36,16 @@ summary_statistics_class = df.groupby("variety").describe()
 # Reference: https://www.digitalocean.com/community/tutorials/numpy-matrix-transpose-array#transpose-of-an-array-like-object
 # Reference: https://www.geeksforgeeks.org/python-pandas-dataframe-to_string/
 
-transpose_summary_class = np.transpose(summary_statistics_class)
-summary_class_string= transpose_summary_class.to_string()
-with open('summary_statistics.txt', 'a') as f:
-    f.write(summary_class_string)
 
+    transpose_summary_class = np.transpose(summary_statistics_class)
+    summary_class_string= transpose_summary_class.to_string()
+
+    print("Here are some summary statistics for each class of Iris flower: \n ")
+    print(summary_class_string)
+
+    f.close()
+
+summary_statistics()
 
 # the first ten rows of the dataframe
 # print(df.head(10))
